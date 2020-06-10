30c10b8d09fbc76788134190850709db42e7a9f5
Sviat <svyatoslav496@gmail.com>
2020-06-11 00:53:27 +0300

    added components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Profile = () => {
+  return(
+    <div className="content">
+      <div>
+        <img src="https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg" alt="" />
+      </div>
+      <div>
+        ava + descr
+      </div>
+      <div>
+        Posts
+          <div>New Post</div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+      </div>
+  </div>
+  )
+}
+
+
+export default Profile
