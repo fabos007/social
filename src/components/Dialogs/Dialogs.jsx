ddea9d327fcac5336ce9070303e6b54ea3d4dd31
Sviat <svyatoslav496@gmail.com>
2020-06-14 15:51:22 +0300

    Dialogs creation (21)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,8 +5,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}> 
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+            Sviat
+        </div>
+        <div className={s.dialog}>
+            Max
+        </div>
+        <div className={s.dialog}>
+            Mark
+        </div>
+        <div className={s.dialog}>
+            Viktor
+        </div>
+        <div className={s.dialog}>
+            John
+        </div>
+        <div className={s.dialog}>
+            Igor
+        </div>
+      </div>
+      <div className={s.messages}>
+          <div className={s.message}>Hi</div>
+          <div className={s.message}>How are you?</div>
+          <div className={s.message}>Your name?</div>
+      </div>
     </div>
   )
 }
