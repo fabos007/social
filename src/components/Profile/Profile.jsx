9b02b8fbbe104f278b2c616b1aad11948ef0c33a
Sviat <svyatoslav496@gmail.com>
2020-06-14 10:24:29 +0300

    added Dialogs/ improved CSS for all file (18)

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
   return(
-    <div className={s.content}>
+    <div>
       <div>
         <img src="https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg" alt="" />
       </div>
