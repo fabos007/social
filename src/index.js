63c144cc6e2c8ad85744412ebf4da6d801180ca4
Sviat <svyatoslav496@gmail.com>
2020-06-17 16:15:55 +0300

    store to hide data (37)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { rerenderEntireTree } from './render';
-import state from './redux/state';
+import store from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
 
+ let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    // <React.StrictMode>
+      <App state={store.getState()} addPost={store.addPost.bind(store)} addMessage={store.addMessage.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} updateMessageText={store.updateMessageText.bind(store)}/>,
+    // </React.StrictMode>
 
-rerenderEntireTree(state);
+    document.getElementById('root')
+  );
+}
+rerenderEntireTree(store.getState());
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+store.subscribe(rerenderEntireTree);
+
+
 serviceWorker.unregister();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,64 +1,79 @@
-import { rerenderEntireTree } from "../render";
+let store = {
+    _state : {
+        profilePage: {
+            posts: [
+                {id:1, message:'Hi, how are you?', likesCount: 15},
+                {id:2, message:'This is my first post', likesCount: 20},
+                {id:3, message: 'This is test', likesCount: 10}
+            ],
+            newPostText: ''
+        },
+        dialogsPage:{
+            messages: [
+                {id:1, message:'Hi'},
+                {id:2, message:'How are you?'},
+                {id:3, message: 'Yo'},
+            ],
+            dialogs:[
+                {id:1, name:'Sviat', avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRg9uScZnPwvrZvwpByA875rVdcQl1a8-FQUdk-DoMBhWL7k1M5&usqp=CAU"},
+                {id:2, name:'Max', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"},
+                {id:3, name:'Mark', avatar: "https://pbs.twimg.com/media/EFEKbx9XUAAZZyg.png"},
+                {id:4, name:'John', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"},
+                {id:5, name:'Igor', avatar: "https://pbs.twimg.com/media/EFEKbx9WwAEPGQM.png"},
+                {id:6, name:'Victor', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"}
+            ],
+            newMessageText: ''
+        },
+        sideBar: {
+            bestFriends: [
+                {id: 10, name: 'Sasha', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"},
+                {id: 11, name: 'Petya', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"},
+                {id: 12, name: 'Dima', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"}
+            ]
+        }
+    }, 
+    _callSubscriber () {
+        console.log('State was changed');
+    },
 
-let state = {
-    profilePage: {
-        posts: [
-            {id:1, message:'Hi, how are you?', likesCount: 15},
-            {id:2, message:'This is my first post', likesCount: 20},
-            {id:3, message: 'This is test', likesCount: 10}
-        ],
-        newPostText: ''
+    getState(){
+        return this._state;
     },
-    dialogsPage:{
-        messages: [
-            {id:1, message:'Hi'},
-            {id:2, message:'How are you?'},
-            {id:3, message: 'Yo'},
-        ],
-        dialogs:[
-            {id:1, name:'Sviat', avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRg9uScZnPwvrZvwpByA875rVdcQl1a8-FQUdk-DoMBhWL7k1M5&usqp=CAU"},
-            {id:2, name:'Max', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"},
-            {id:3, name:'Mark', avatar: "https://pbs.twimg.com/media/EFEKbx9XUAAZZyg.png"},
-            {id:4, name:'John', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"},
-            {id:5, name:'Igor', avatar: "https://pbs.twimg.com/media/EFEKbx9WwAEPGQM.png"},
-            {id:6, name:'Victor', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"}
-        ],
-        newMessageText: ''
+    subscribe(observer){
+        this._callSubscriber = observer   
     },
-    sideBar: {
-        bestFriends: [
-            {id: 10, name: 'Sasha', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"},
-		    {id: 11, name: 'Petya', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"},
-		    {id: 12, name: 'Dima', avatar: "https://memepedia.ru/wp-content/uploads/2018/06/cover-2-2-768x507.jpg"}
-        ]
+ 
+    addPost () {
+        let newPost = {id:5, message: this._state.profilePage.newPostText, likesCount: 0}
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText){
+        this._state.profilePage.newPostText = newText;  
+        this._callSubscriber(this._state);
+    },
+    addMessage(message){
+        let newMessage = {id: 1, message: this._state.dialogsPage.newMessageText};
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessageText = ''
+        this._callSubscriber(this._state);
+    },
+    updateMessageText(message){
+        this._state.dialogsPage.newMessageText = message;
+        
+        this._callSubscriber(this._state);
+    },
+
+    dispatch(action){
+
     }
 }
 
-export let addPost = () => {
-    let newPost = {id:5, message: state.profilePage.newPostText, likesCount: 0}
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state);
-};
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;  
-    rerenderEntireTree(state);
-};
-
-export let addMessage = (message) => {
-    let newMessage = {id: 1, message: state.dialogsPage.newMessageText};
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessageText = ''
-    rerenderEntireTree(state);
-}
-
-export let updateMessageText = (message) => {
-    state.dialogsPage.newMessageText = message;
-    
-    rerenderEntireTree(state);
-}
 
 
 
-export default state;
+
+export default store;
+
+window.store = store;
